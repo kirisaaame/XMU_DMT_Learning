cfaa6f157a1467a7eb99ac488e0b00a08c7ec2f6
kirisaaame <1662552356@qq.com>
2024-06-27 10:18:11 +0900

    例行更新

--- 其他课程/Java入门（日文）/Java演習(解答）/javalec10/javalec10/CloneTest.java
+++ 其他课程/Java入门（日文）/Java演習(解答）/javalec10/javalec10/CloneTest.java
@@ -1,16 +1,31 @@
 package javalec10;
-
-class Chocolate {
+/*
+ * 3年　15組　96番　ゾウ　ジファン
+ * 出力：
+ * C(9)C(1)C(2)
+	C(0)C(1)C(2)
+ */
+class Chocolate implements Cloneable{
 	private int flavor;
 	Chocolate(int f) { flavor = f; }
+	public Chocolate clone() {
+		return new Chocolate(flavor);
+	}
 	public String toString() { return "C(" + flavor + ")"; }
 	public void set_flavor(int f) { flavor = f; }
 }
 
-class Box {
+class Box implements Cloneable{
 	Chocolate piece[];
 	Box(Chocolate[] c) {
 		piece = c;
+	}
+	public Box clone() {
+		Chocolate[] newpiece = new Chocolate[piece.length];
+		for (int i = 0; i < piece.length; i++) {
+			newpiece[i] = piece[i].clone();
+		}
+		return new Box(newpiece);
 	}
 	public String toString() {
 		String ret = "";
@@ -28,10 +43,8 @@ public class CloneTest {
 		Box sample = new Box(choco);
 		
 		// boxには最初、sampleと同じチョコが入っているようにする（コピーする）
-		Box box; //XXX
-		
-		// boxの0個目のチョコについて、味を9に変更
-		//XXX
+		Box box = sample.clone() ;
+		box.piece[0].set_flavor(9);
 		
 		// box, sampleを表示するとどうなるか？
 		System.out.println(box);
